5c81f758b266ecf1090d394e3f9fd8cceef6c571
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-02-11 12:53:23 +0300

    init

---
+++ src/main/java/ru/helpanimals/animalsbot/AnimalsBotApplication.java
@@ -0,0 +1,13 @@
+package ru.helpanimals.animalsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/helpanimals/animalsbot/AnimalsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.helpanimals.animalsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
